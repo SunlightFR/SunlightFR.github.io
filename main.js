fb7d5a6cd9a7c55f561a135ef2c5e75eb03faac0
gaetan peron <gperon@e103m06.istic.univ-rennes1.fr>
2023-09-19 09:14:26 +0200

    Fini les classes Formes

--- main.js
+++ main.js
@@ -12,9 +12,11 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
 /////
 
 // Code temporaire pour tester l'affiche de la vue
-//var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
-//rec.paint(ctx);
-//var ligne = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
+var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
+console.log(rec)
+// rec.paint(ctx);
+var ligne = new Line(10, 20, 50, 100, 5, '#00CCC0');
+console.log(ligne)
 //ligne.paint(ctx);
 // tester également Dessin.
 ////
--- model.js
+++ model.js
@@ -1,3 +1,34 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Forme(couleur, epaisseur){
+    Forme.prototype.couleur = couleur
+    Forme.prototype.epaisseur = epaisseur
+}
+
+function Line(p1x,p1y,p2x,p2y,couleur,epaisseur){
+    Forme.call(this,couleur,epaisseur)
+    this.p1x = p1x
+    this.p1y = p1y
+    this.p2x = p2x
+    this.p2y = p2y
+}
+Line.prototype = new Forme()
+
+function Rectangle(px, py , largeur, longueur,couleur,epaisseur){
+    Forme.call(this,couleur,epaisseur)
+    this.px = px
+    this.py = py
+    this.largeur = largeur
+    this.longueur = longueur
+}
+
+Rectangle.prototype = new Forme()
+
+function Drawing(){
+    this.formes = []
+    this.add = forme => {
+        this.formes.push(forme)
+    }
+}
